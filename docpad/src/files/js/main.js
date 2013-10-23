1608623ab7c98255b6b3349da9357f5ed183494c
Mark Crawley <mark@crawleyweb.co.uk>
2013-10-23 19:31:15 +0100

    Created image carousel

--- docpad/src/files/js/main.js
+++ docpad/src/files/js/main.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-	$('a[href^="mailto"]').click(function(){_gaq.push(['_trackEvent', 'email', 'send', this.href.replace(/^mailto:/i, ''),,true]);});
+	var i = 0;
+    var duration = 500;
+    var interval = 5000;
+
+    var images = ['binc_monkey', 'binc_rose', 'binc_crab', 'binc_horse', 'binc_fish', 'binc_pig', 'binc_frog', 'binc_lion'];
+    switchImg();
+
+    function switchImg() {
+	    $(".content-img")
+	        .attr("src", "images/carousel/" + (images[i<7?++i:(i=1,i)]) + ".jpg")
+	        .css("opacity", 0)
+	        .prependTo(".content-img-wrapper")
+	        .animate({
+	            opacity: 1
+	        }, {
+	            duration: duration
+	        })
+	        .next()
+	        .animate({
+	            opacity: 0
+	        }, {
+	            duration: duration
+	        })
+	        .promise()
+	        .done(function () {
+	            $(this).remove();
+	            setTimeout(switchImg, interval);
+	        });
+    }
 });
