05137d62d36f23e4731ad37d2b96654c3a932faf
Mark Crawley <mark@crawleyweb.co.uk>
2015-07-25 14:11:08 +0100

    Random testimonials

--- src/files/js/main.js
+++ src/files/js/main.js
@@ -3,22 +3,25 @@ $(document).ready(function() {
 
 	// Setup carousel rotation
 	var i = 0;
+	var j = 0;
     var duration = 1000;
     var interval = 6000;
 
     var images = 'carousel-1 carousel-2 carousel-3 carousel-4 carousel-5';
-	var arr = images.split(' ');
+	var cArr = images.split(' ');
 	// Preload the images for best performance
-	$(arr).each(function(){$("<img/>")[0].src="img/carousel/"+this+".jpg"});
+	$(cArr).each(function(){$("<img/>")[0].src="img/carousel/"+this+".jpg"});
+
+	var testimonials = 'I made an excellent choice in choosing Rich to provide entertainment for our event. It created a lasting impression and is definitely one to repeat.|Rich was brilliant at my daughter\'s birthday party, she loved it!|Thanks for a super balloon making session yesterday at my son\'s party, he loved his Thomas!|Had the pleasure of working alongside Rich from Balloons Inc. at the weekend - I have to say, he is superb!';
+	var tArr = testimonials.split('|');
 
 	// Start the rotation
     setTimeout(switchImg, interval);   
 
     function switchImg() {
+    	var cNext = i<cArr.length-1?++i:(i=0,i);
 	    $('.carousel')
-	    	// .removeClass(images)
-	        // .addClass(arr[i<4?++i:(i=0,i)])
-	        .css('background-image', 'url("img/carousel/' + arr[i<4?++i:(i=0,i)] + '.jpg")')
+	    	.css('background-image', 'url("img/carousel/' + cArr[cNext] + '.jpg")')
 	        .css('opacity', 0.5)
 	        .animate({
 	            opacity: 0.7
@@ -35,6 +38,11 @@ $(document).ready(function() {
 	            setTimeout(switchImg, interval);
 	        })
 	    ;
+
+	    var tNext = j<tArr.length-1?++j:(j=0,j);
+	    $('.testimonial p').text('"' + tArr[tNext] + '"');
+
+	    // console.log('C'+cNext+' T'+tNext);
     }
 
     // Contact form validation
