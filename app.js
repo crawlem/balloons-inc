8baaac3796ab9fafacafe1cccc948bec962b61ed
Mark Crawley <mark.crawley@gmail.com>
2016-09-04 14:19:55 +0100

    Force content reload each request

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var	express 		= require('express'),
 	SaveStatic 		= require('save-static');
  
 // initialize with the root path for saving to disk 
-var staticPath = __dirname + '/public'; // <= for example 
+var staticPath = __dirname + '/out'; // <= for example 
 var saveStatic = new SaveStatic(staticPath);
 
 handlebars.registerHelper('equal', function(lvalue, rvalue, options) {
@@ -34,76 +34,10 @@ handlebars.registerHelper('md', function(markdown, options) {
 // Create Express app
 var app = express();
 
-// Setup Contentful
-var client = contentful.createClient({
-	space: 'gnnn40ybavjk',
-	accessToken: '2d9e7ca654b74b162955d05cb18dfef79cb0e13a2608cc50173abe2ff6301305'
-});
-// Retrieve CMS content
-var home, pages, sitemap, contact, mainMenu, footerMenu, testimonials;
-loadContent();
-
-function loadContent() {
-	console.log('Reloading CMS content');
-
-	client.getEntries({'content_type': 'home'}).then(function(entries) {
-		entries.items.forEach(function(entry) {
-			home = entry;
-		});
-	});
-	client.getEntries({'content_type': 'sitemap'}).then(function(entries) {
-		entries.items.forEach(function(entry) {
-			sitemap = entry;
-		});
-	});
-	client.getEntries({'content_type': 'contact'}).then(function(entries) {
-		entries.items.forEach(function(entry) {
-			contact = entry;
-		});
-	});
-	client.getEntries({'content_type': 'page'}).then(function(entries) {
-		pages = entries;
-		pages.items.forEach(function(entry) {
-			app.get('/' + entry.fields.alias + '.html', function (req, res) {
-				loadContent();
-				res.render('text', { page: entry, mainMenu: mainMenu, footerMenu: footerMenu}, saveStatic(res));
-			});
-		});
-	});
-	client.getEntries({'content_type': 'menu', 'fields.alias': 'main-menu'}).then(function(entries) {
-		mainMenu = entries;
-	});
-	client.getEntries({'content_type': 'menu', 'fields.alias': 'footer-menu'}).then(function(entries) {
-		footerMenu = entries;
-	});
-	client.getEntries({'content_type': 'testimonial'}).then(function(entries) {
-		testimonials = entries;
-		app.get('/js/testimonials.js', function (req, res) {
-			// var output = 'var testimonials = ';
-			// testimonials.items.forEach(function(entry) {
-			// 	output += "'" + entry.fields.quote.replace("'", "\\'") + "'";
-			// });
-			// output = output.replace("''", "' + '|");
-			// output += ';\n'
-
-			// output += 'var testimonalLinks = ';
-			// testimonials.items.forEach(function(entry) {
-			// 	output += "'" + entry.fields.link + "'";
-			// });
-			// output = output.replace("''", "' + '|");
-			// output += ';'
-
-			res.set('Content-Type', 'application/javascript');
-			res.render('testimonials', { layout: 'js', testimonials: testimonials }, saveStatic(res));
-			// res.send(output);
-		});
-	});
-}
-
-
 // Setup Handlebars template
 app.engine('handlebars', hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
 // Setup SASS pre-processing
 app.use(sassMiddleware({
     /* Options */
@@ -111,25 +45,87 @@ app.use(sassMiddleware({
     dest: path.join(__dirname, 'public', 'css'),
     prefix: '/css'
 }));
+
 // Setup static file routing
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Contentful API client
+var client = contentful.createClient({
+	space: 'gnnn40ybavjk',
+	accessToken: '2d9e7ca654b74b162955d05cb18dfef79cb0e13a2608cc50173abe2ff6301305'
+});
+
 // Serve pages
-app.get('/', function (req, res) {
-	loadContent();
-	res.render('index', { page: home, mainMenu: mainMenu, footerMenu: footerMenu}, saveStatic(res));
+app.get(['/', '/index.html'], function(req, res) {
+	console.log('Loading ' + req.originalUrl);
+	client.getEntries({'content_type': 'home'}).then(function(entries) {
+		var entry = entries.items[0];
+		client.getEntries({'content_type': 'menu', 'fields.alias': 'main-menu'}).then(function(entries) {
+			var mainMenu = entries;
+			client.getEntries({'content_type': 'menu', 'fields.alias': 'footer-menu'}).then(function(entries) {
+				var footerMenu = entries;
+				res.render('index', { page: entry, mainMenu: mainMenu, footerMenu: footerMenu}, saveStatic(res));
+			});
+		});
+		
+	});
 });
-app.get('/index.html', function (req, res) {
-	loadContent();
-	res.render('index', { page: home, mainMenu: mainMenu, footerMenu: footerMenu}, saveStatic(res));
+
+app.get('/sitemap.html', function(req, res) {
+	console.log('Loading ' + req.originalUrl);
+	client.getEntries({'content_type': 'sitemap'}).then(function(entries) {
+		var entry = entries.items[0];
+		client.getEntries({'content_type': 'menu', 'fields.alias': 'main-menu'}).then(function(entries) {
+			var mainMenu = entries;
+			client.getEntries({'content_type': 'menu', 'fields.alias': 'footer-menu'}).then(function(entries) {
+				var footerMenu = entries;
+				res.render('sitemap', { page: entry, mainMenu: mainMenu, footerMenu: footerMenu}, saveStatic(res));
+			});
+		});
+		
+	});
 });
-app.get('/sitemap.html', function (req, res) {
-	loadContent();
-	res.render('sitemap', { page: sitemap, mainMenu: mainMenu, footerMenu: footerMenu}, saveStatic(res));
+
+app.get('/contact.html', function(req, res) {
+	console.log('Loading ' + req.originalUrl);
+	client.getEntries({'content_type': 'contact'}).then(function(entries) {
+		var entry = entries.items[0];
+		client.getEntries({'content_type': 'menu', 'fields.alias': 'main-menu'}).then(function(entries) {
+			var mainMenu = entries;
+			client.getEntries({'content_type': 'menu', 'fields.alias': 'footer-menu'}).then(function(entries) {
+				var footerMenu = entries;
+				res.render('contact', { page: entry, mainMenu: mainMenu, footerMenu: footerMenu}, saveStatic(res));
+			});
+		});
+		
+	});
 });
-app.get('/contact.html', function (req, res) {
-	loadContent();
-	res.render('contact', { page: contact, mainMenu: mainMenu, footerMenu: footerMenu}, saveStatic(res));
+
+app.get('/js/testimonials.js', function(req, res) {
+	console.log('Loading ' + req.originalUrl);
+	client.getEntries({'content_type': 'testimonial'}).then(function(entries) {
+		res.set('Content-Type', 'application/javascript');
+		res.render('testimonials', { layout: 'js', testimonials: entries }, saveStatic(res));
+	});
+});
+
+// The rest of the "normal" pages
+client.getEntries({'content_type': 'page'}).then(function(pages) {
+	pages.items.forEach(function(entry) {
+		console.log('Setting up GET address for ' + entry.fields.alias);
+		app.get('/' + entry.fields.alias + '.html', function(req, res) {
+			console.log('Loading ' + req.originalUrl);
+			client.getEntries({'content_type': 'page', 'fields.alias': entry.fields.alias}).then(function(page) {
+				client.getEntries({'content_type': 'menu', 'fields.alias': 'main-menu'}).then(function(mainMenus) {
+					var mainMenu = mainMenus;
+					client.getEntries({'content_type': 'menu', 'fields.alias': 'footer-menu'}).then(function(footerMenus) {
+						var footerMenu = footerMenus;
+						res.render('text', { page: page.items[0], mainMenu: mainMenu, footerMenu: footerMenu}, saveStatic(res));
+					});
+				});
+			});
+		});
+	});
 });
 
 // Bind HTTP server to port
